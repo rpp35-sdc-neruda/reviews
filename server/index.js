f7ece46fc0d4a2d30aa9d488af350ae4e498eab3
Nourse41 <Nourse41@gmail.com>
2022-06-23 14:02:42 -0400

    test suite installed

--- server/index.js
+++ server/index.js
@@ -2,7 +2,19 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('Sup World')
 })
 
 app.listen(3000)
+
+
+// TODO:  GET /reviews/ -- returns list of all reviews for given product/Response = Status: 200 OK
+
+// TODO: GET /reviews/meta -- returns review metadata for a given product/Response = Status: 200 OK
+
+// TODO: POST /reviews -- adds a review for given product/Response = 201 CREATED
+
+// TODO: PUT /reviews/:review_id/helpful -- mark review helpful by review_id/Response = Status: 204 NO CONTENT
+
+// TODO: PUT /reviews/:review_id/report -- report a review by review_id/Response = Status: 204 NO CONTENT
+
