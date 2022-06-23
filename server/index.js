abf2be84e77f09e8b76d1800f562b0c7087fdf85
Nourse41 <Nourse41@gmail.com>
2022-06-23 14:44:00 -0400

    created basic routes

--- server/index.js
+++ server/index.js
@@ -1,20 +1,34 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
-  res.send('Sup World')
-})
+  res.send('Hello World')
+});
 
-app.listen(3000)
+// GET /reviews/ -- returns list of all reviews for given product/Response = Status: 200 OK
+app.get('/reviews', (req, res) => {
+  res.status(200).send('Reviews');
+});
 
+// GET /reviews/meta -- returns review metadata for a given product/Response = Status: 200 OK
+app.get('/reviews/meta', (req, res) => {
+  res.status(200).send('Meta');
+});
 
-// TODO:  GET /reviews/ -- returns list of all reviews for given product/Response = Status: 200 OK
+// POST /reviews -- adds a review for given product/Response = 201 CREATED
+app.post('/reviews', (req, res) => {
+  res.status(201).send('Created');
+});
 
-// TODO: GET /reviews/meta -- returns review metadata for a given product/Response = Status: 200 OK
+//! TPUT /reviews/:review_id/helpful -- mark review helpful by review_id/Response = Status: 204 NO CONTENT
+app.put('/reviews/:review_id/helpful', (req, res) => {
+  res.status(204).send(req.params.review_id);
+});
 
-// TODO: POST /reviews -- adds a review for given product/Response = 201 CREATED
+//! PUT /reviews/:review_id/report -- report a review by review_id/Response = Status: 204 NO CONTENT
+app.put('/reviews/:review_id/report', (req, res) => {
+  res.status(204).send(req.params.review_id);
+});
 
-// TODO: PUT /reviews/:review_id/helpful -- mark review helpful by review_id/Response = Status: 204 NO CONTENT
-
-// TODO: PUT /reviews/:review_id/report -- report a review by review_id/Response = Status: 204 NO CONTENT
-
+app.listen(port);
